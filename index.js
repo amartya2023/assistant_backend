10819c8d30f996169e3388d90130ab66a67ab82e
Amartya Ghosh <amartyaghosh48@gmail.com>
2025-09-07 23:24:37 +0530

    add frontend link using cors

--- index.js
+++ index.js
@@ -3,20 +3,24 @@ import dotenv from "dotenv";
 import connectDb from "./config/db.js";
 import authRouter from "./routes/auth.routes.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 dotenv.config();
 
-
 const app = express();
-const port=process.env.PORT || 5000
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+const port = process.env.PORT || 5000;
 
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 
-app.use("/api/auth", authRouter)
+app.use("/api/auth", authRouter);
 
-
-app.listen(port,() => {
-    connectDb();
-    console.log("Server started");
-    
-})
+app.listen(port, () => {
+  connectDb();
+  console.log("Server started");
+});
